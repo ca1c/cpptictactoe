e51ff957c02d03804f25df2f446f51af5601607a
ca1c <gegs921@gmail.com>
2024-01-27 22:29:09 -0800

    added minimax algorithm to GameState class

--- GameState.cpp
+++ GameState.cpp
@@ -15,7 +15,7 @@ vector<int> GameState::GetBoard() {
     return board;
 }
 
-vector<int> GameState::GetEmptySpots() {
+vector<int> GameState::GetEmptySpots(vector<int> board) {
     vector<int> emptySpots;
 
     for(vector<int>::iterator i = board.begin(); i != board.end(); i++) {
@@ -27,7 +27,7 @@ vector<int> GameState::GetEmptySpots() {
     return emptySpots;
 }
 
-bool GameState::PlayerWins(int player) {
+bool GameState::PlayerWins(int player, vector<int> board) {
     return ((board.at(0) == player && board.at(1) == player && board.at(2) == player) ||
             (board.at(3) == player && board.at(4) == player && board.at(5) == player) ||
             (board.at(6) == player && board.at(7) == player && board.at(8) == player) ||
@@ -44,4 +44,75 @@ int GameState::GetP1() {
 
 int GameState::GetP2() {
     return p2;
+}
+
+int GameState::AIBestMove() {
+    vector<int> emptySpots = GetEmptySpots(board);
+    vector<int> moveScores = GetMoveScores(board, currP, currP);
+    return emptySpots.at(GetMaxIndex(moveScores));
+}
+
+vector<int> GameState::GetMoveScores(vector<int> board, int currPlayer, int origPlayer) {
+    vector<int> emptySpots = GetEmptySpots(board);
+    vector<int> moveScores;
+ 
+    if(PlayerWins(currPlayer, board) && currPlayer == origPlayer) {
+        moveScores.push_back(1);
+        return moveScores;
+    }
+    else if(PlayerWins(currPlayer, board) && currPlayer != origPlayer) {
+        moveScores.push_back(-1);
+        return moveScores;
+    } 
+    else if(emptySpots.size() == 0) {
+        moveScores.push_back(0);
+        return moveScores;
+    }
+
+    for(vector<int>::iterator i = emptySpots.begin(); i != emptySpots.end(); i++) {
+        board.at((*i)) = currPlayer; 
+
+        int score;
+        if(currPlayer == 1) {
+            score = GetMoveScores(board, 2, origPlayer).at(0);
+        }
+        else if(currPlayer == 2) {
+            score = GetMoveScores(board, 1, origPlayer).at(0);
+        }
+
+        board.at((*i)) = 0;
+
+        moveScores.push_back(score);
+    }
+
+    return moveScores;
+
+}
+
+int GameState::GetMaxIndex(vector<int> scores) {
+    int max = scores.at(0);
+    int maxIndex = 0;
+
+    for(vector<int>::iterator i = scores.begin(); i != scores.end(); i++) {
+        if((*i) > max) {
+            max = (*i);
+            maxIndex = i - scores.begin();
+        }
+    }
+
+    return maxIndex;
+}
+
+int GameState::GetMinIndex(vector<int> scores) {
+    int min = scores.at(0);
+    int minIndex = 0;
+
+    for(vector<int>::iterator i = scores.begin(); i != scores.end(); i++) {
+        if((*i) < min) {
+            min = (*i);
+            minIndex = i - scores.begin();
+        }
+    }
+
+    return minIndex;
 }
--- GameState.h
+++ GameState.h
@@ -15,11 +15,15 @@ class GameState {
     public:
         GameState(int initP);
         vector<int> GetBoard();
-        vector<int> GetEmptySpots();
-        bool PlayerWins(int player);
+        vector<int> GetEmptySpots(vector<int> board);
+        bool PlayerWins(int player, vector<int> board);
         void MakeMove(int index);
         int GetP1();
         int GetP2();
+        int AIBestMove();
+        vector<int> GetMoveScores(vector<int> board, int currPlayer, int origPlayer);
+        int GetMaxIndex(vector<int> scores);
+        int GetMinIndex(vector<int> scores);
 };
 
 #endif
